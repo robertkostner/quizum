57e60b56cb5c8edc0210caa7d2669c7752e07069
robertkostner <robert.kostner@volny.cz>
2021-04-27 16:02:10 +0000

    dvě stranky

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Link from 'next/link';
+
 const HomePage = () => {
     return <div>
-        <h1>Pokus o kvíz</h1>
-        
+        <h1>Kvízy</h1>
+            <Link href="/kviz1"><a>Kvíz 1</a></Link>
         </div>
   }
   
---
+++ pages/kviz1.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const Kviz1Page = () => {
+    return <div>
+        <h1>Kvíz č. 1</h1>
+            <Link href="/"><a>Zpět na HP</a></Link>
+        </div>
+  }
+  
+  export default Kviz1Page
